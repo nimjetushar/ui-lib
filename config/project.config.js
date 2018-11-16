d63c64cfb0d5c22e9dab4a6b7fe578a273060fe9
tusharnimje21@gmail.com <tusharnimje21@gmail.com>
2018-11-16 17:59:48 +0530

    initial commit

---
+++ config/project.config.js
@@ -0,0 +1,16 @@
+const Config = {
+  envTypes: {
+    DEVELOPMENT: "development",
+    TEST: "test",
+    PRODUCTION: "production"
+  },
+  devServer: {
+    port: 5200,
+    defaultPort: 5200
+  },
+  srcDirName: "src",
+  distDirName: "dist",
+  docDirName: "docs"
+};
+
+export default Config;
