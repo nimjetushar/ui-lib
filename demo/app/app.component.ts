6a1098f3e8f7b581321c3cc933665f024a05c2f8
tusharnimje21@gmail.com <tusharnimje21@gmail.com>
2018-12-16 11:57:06 +0530

    updated dependancy

--- demo/app/app.component.ts
+++ demo/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "demo-root",
-  template: "<h1>Demo</h1>"
+  template: "<h1>Demo</h1>",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {}
