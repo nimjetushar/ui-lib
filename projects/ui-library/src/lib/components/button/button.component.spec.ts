367594455cfa9bc6b784eb061b2bbe8d586e2050
tusharnimje21@gmail.com <tusharnimje21@gmail.com>
2019-03-28 23:10:51 +0530

    fixed test case  issues

--- projects/ui-library/src/lib/components/button/button.component.spec.ts
+++ projects/ui-library/src/lib/components/button/button.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ButtonComponent } from './button.component';
+import { BadgeComponent } from './../badge/badge.component';
 
 describe('ButtonComponent', () => {
   let component: ButtonComponent;
@@ -8,7 +9,7 @@ describe('ButtonComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ButtonComponent ]
+      declarations: [ ButtonComponent, BadgeComponent ]
     })
     .compileComponents();
   }));
