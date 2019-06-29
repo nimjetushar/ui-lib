f823c92bcc7b4ca707e96645c563e7f1747f8f82
tusharnimje21@gmail.com <tusharnimje21@gmail.com>
2019-06-29 23:59:16 +0530

    updated checkbox demo

--- src/app/demo/components/checkbox/checkbox.component.ts
+++ src/app/demo/components/checkbox/checkbox.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Options } from '../../common/demo-wrapper/demo-wrapper.component';
 
 @Component({
   selector: 'app-checkbox',
@@ -7,11 +8,36 @@ import { Component } from '@angular/core';
 })
 export class CheckboxComponent {
 
-  compSyntax: string = `<t-checkbox label="Checkbox"></t-checkbox>`;
+  compSyntax: string = `<t-checkbox label="Checkbox" (handleChange)="handleChange($event)"></t-checkbox> \n
+  <t-checkbox label="Checkbox" disabled="true"></t-checkbox>`;
+
+  public options: Options = {
+    name: 't-checkbox',
+    options: [
+      {
+        parameter: 'label',
+        type: 'string',
+        desc: 'checkbox label'
+      },
+      {
+        parameter: 'disabled',
+        default: 'false',
+        type: 'boolean',
+        desc: 'Disable button'
+      }
+    ],
+    methods: [
+      {
+        method: 'handleChange',
+        param: ['event: boolean'],
+        desc: 'triggers event on checkbox value change'
+      }
+    ]
+  };
 
   constructor() { }
 
-  handleChange(event:any): void {
+  handleChange(event: any): void {
     console.log(event);
   }
 }
