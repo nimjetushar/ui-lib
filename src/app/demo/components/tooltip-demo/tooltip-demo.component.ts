a115e64f46348b881b20546428ca9ae0e9447c69
tusharnimje21@gmail.com <tusharnimje21@gmail.com>
2019-07-09 22:02:35 +0530

    added tooltip demo comp

---
+++ src/app/demo/components/tooltip-demo/tooltip-demo.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Options } from '../../common/demo-wrapper/demo-wrapper.component';
+
+@Component({
+  selector: 'app-tooltip-demo',
+  templateUrl: './tooltip-demo.component.html',
+  styleUrls: ['./tooltip-demo.component.scss']
+})
+export class TooltipDemoComponent {
+
+  compSyntax: string = ` <span tooltip="content to be shown in the tooltip" 
+  [tooltipDisabled]="false"
+  [tooltipAnimation]="true"
+  tooltipPlacement="bottom"
+  >Element on which tooltip is added  </span>`;
+
+  options: Options = {
+    name: 'tooltip',
+    componentType: 'Directive',
+    options: [
+      {
+        parameter: 'tooltip',
+        type: 'string | TooltipContent',
+        desc: 'content to be displayed inside tooltip'
+      },
+    ]
+  };
+
+  constructor() { }
+
+}
