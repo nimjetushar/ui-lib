c34708e790a21ef588ff643e3dd0dcf258a49dd9
tusharnimje21@gmail.com <tusharnimje21@gmail.com>
2019-08-08 08:49:21 +0530

    fixed lint issue

--- src/app/demo/styles/grid/grid.component.ts
+++ src/app/demo/styles/grid/grid.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-grid',
   templateUrl: './grid.component.html',
   styleUrls: ['./grid.component.scss']
 })
-export class GridComponent implements OnInit {
+export class GridComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
