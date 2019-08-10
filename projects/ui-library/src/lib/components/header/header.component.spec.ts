02b0afc8be86972acdfe47294fe8ff3617d8c806
tusharnimje21@gmail.com <tusharnimje21@gmail.com>
2019-08-11 01:22:27 +0530

    fixed test case issue

--- projects/ui-library/src/lib/components/header/header.component.spec.ts
+++ projects/ui-library/src/lib/components/header/header.component.spec.ts
@@ -1,3 +1,4 @@
+import { RouterTestingModule } from '@angular/router/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
@@ -9,7 +10,8 @@ describe('HeaderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [HeaderComponent]
+      declarations: [HeaderComponent],
+      imports: [RouterTestingModule]
     })
       .compileComponents();
   }));
