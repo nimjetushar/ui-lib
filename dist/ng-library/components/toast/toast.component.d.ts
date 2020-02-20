a01bfc293d372ba3edbae804a55406c108b81ee8
Tushar <tusharnimje21@gmail.com>
2020-02-20 20:25:56 +0530

    updated package to 20.2.20

--- dist/ng-library/components/toast/toast.component.d.ts
+++ dist/ng-library/components/toast/toast.component.d.ts
@@ -1,5 +1,5 @@
 export declare class ToastComponent {
-    position?: 'top-right' | 'top-left' | 'bottom-right' | 'bottom-left' | 'top-center' | 'bottom-center' | 'center';
+    position: 'top-right' | 'top-left' | 'bottom-right' | 'bottom-left' | 'top-center' | 'bottom-center' | 'center';
     baseZIndex: number;
     key: string;
 }
