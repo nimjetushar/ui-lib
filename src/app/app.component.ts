effb26117371ba8b8294d6e83b02ee988291db45
Tushar Nimje <tusharnimje21@gmail.com>
2023-03-28 21:58:36 +0530

    added nx

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ui-library-documentation-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ui-library-documentation';
+}
