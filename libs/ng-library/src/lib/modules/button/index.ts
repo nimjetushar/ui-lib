7ce1c227c601b6819608e5bf3162191d8451cff4
Tushar Nimje <tusharnimje21@gmail.com>
2023-11-02 23:08:12 +0530

    fixed build issue

--- libs/ng-library/src/lib/modules/button/index.ts
+++ libs/ng-library/src/lib/modules/button/index.ts
@@ -1,3 +1,4 @@
 export { ButtonComponent } from './button.component';
+export { ButtonDirective } from './button.directive';
 export { ButtonModule } from './button.module';
 export { ButtonType } from './type';
