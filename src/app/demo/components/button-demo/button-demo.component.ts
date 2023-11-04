b25a0da5e794cdd08c0186858627e9f734405a06
Tushar Nimje <tusharnimje21@gmail.com>
2023-11-04 10:58:54 +0530

    updated button component

--- src/app/demo/components/button-demo/button-demo.component.ts
+++ src/app/demo/components/button-demo/button-demo.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ButtonComponent } from '@fourjs/ng-library';
 
-import { DemoParameters, Options } from '../../../common';
+import { DemoParameters, Options, getComponentParameter } from '../../../common';
 
 @Component({
   selector: 'ui-library-documentation-button-demo',
@@ -12,43 +12,42 @@ import { DemoParameters, Options } from '../../../common';
 export class ButtonDemoComponent implements DemoParameters<ButtonComponent> {
   readonly componentSyntax: string[] = ['<t-button label="Home" type="primary" />'];
   readonly importSyntax = 'import { ButtonModule } from "@fourjs/ng-library";';
-
-  readonly options: Options<ButtonComponent> = {
-    name: 't-button',
-    options: [
-      {
-        parameter: 'label',
-        type: 'string',
-        description: 'Button label',
-      },
-      {
-        parameter: 'type',
-        type: 'primary | secondary',
-        description: 'Type of button, valid values are primary, secondary',
-        default: 'primary',
-      },
-      {
-        parameter: 'size',
-        type: 'normal | large | small',
-        default: 'normal',
-        description: 'Increases button size when true',
-      },
-      {
-        parameter: 'disabled',
-        default: 'false',
-        type: 'boolean',
-        description: 'Disabled button',
-      },
-      {
-        parameter: 'link',
-        default: 'false',
-        type: 'boolean',
-        description: 'Add a link style to the button.',
-      },
-    ],
-  };
+  readonly options = getButtonComponentOptions();
 
   clickHandler(): void {
     alert('button clicked !!!!');
   }
 }
+
+const getButtonComponentOptions = (): Options<ButtonComponent> => {
+  const instance = new ButtonComponent();
+  const getParameter = getComponentParameter(instance);
+
+  return {
+    name: 't-button',
+    options: [
+      getParameter({
+        parameter: 'label',
+        description: 'Button label',
+      }),
+      getParameter({
+        parameter: 'type',
+        type: 'primary | secondary',
+        description: 'Type of button, valid values are primary, secondary',
+      }),
+      getParameter({
+        parameter: 'size',
+        type: 'normal | large | small',
+        description: 'Increases button size when true',
+      }),
+      getParameter({
+        parameter: 'disabled',
+        description: 'Disabled button',
+      }),
+      getParameter({
+        parameter: 'link',
+        description: 'Add a link style to the button.',
+      }),
+    ],
+  };
+};
